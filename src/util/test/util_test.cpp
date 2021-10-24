de6e7cb53670f32fad9a926f2a76a470842d8fc0
martin4861 <martin4861@gmx.de>
2021-10-24 11:09:11 +0200

    Added dep to gtest and added tests

---
+++ src/util/test/util_test.cpp
@@ -0,0 +1,12 @@
+#include <gmock/gmock.h>
+#include "util/util.hpp"
+
+TEST(Foo, Bar) 
+{
+    EXPECT_THAT(true, ::testing::IsTrue());
+}
+
+TEST(Foo, Pow2)
+{
+    EXPECT_THAT(pow2(3.0), ::testing::Eq(3.0*3.0));
+}
